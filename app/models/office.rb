361b4c68372d8e7f8c658b6a8f7b0b6f67042e8c
Raul Gil <rgil@Rauls-MacBook-Pro.local>
2014-01-15 18:28:32 -0500

    users scaffold

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,5 +1,6 @@
 class Office < ActiveRecord::Base
   has_one :geo
+  belongs_to :user
   accepts_nested_attributes_for :geo_attributes
   attr_accessible :name, :geo_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140115231734) do
+ActiveRecord::Schema.define(version: 20140115232510) do
 
   create_table "geos", force: true do |t|
     t.string   "name"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20140115231734) do
 
   create_table "teams", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "full_name"
+    t.string   "email"
+    t.string   "password"
+    t.integer  "office_id"
+    t.integer  "team_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
