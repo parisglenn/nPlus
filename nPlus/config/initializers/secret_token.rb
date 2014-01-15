16788b40d14162bab540511da5a12de49631d0b6
Erik Osmond <erikosmond@gmail.com>
2014-01-15 18:00:23 -0500

    initial commit

---
+++ nPlus/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NPlus::Application.config.secret_token = '1a9202a9ad9336e06af752d6ec6bfdbf5195cf2931b9f4a2152f0476904129f934d8752a9622b084edd0494156083d220370f53d53e36c2c1e9d70baf17b5b86'
