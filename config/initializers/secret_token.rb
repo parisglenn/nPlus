fdf718d6fdbbb508c76d0a5b01a48c8e38c125ef
Raul Gil <rgil@Rauls-MacBook-Pro.local>
2014-01-15 20:11:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NPlus::Application.config.secret_token = '640f19171396b561504906c73abd38afb08e2f8d54a1670e45462fec36ebad275e573b94fe167989dbc904fa57e15e11ffffaa298ff1ddfa863210fad88ce21c'
