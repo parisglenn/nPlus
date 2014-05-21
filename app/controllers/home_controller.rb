c7a00b637719d8c88a6eee7eea62e0d7161d9791
Erik Osmond <erikosmond@gmail.com>
2014-05-20 20:40:56 -0400

    updating bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,16 @@ class HomeController < ApplicationController
 	before_filter :authenticate_user!
   def index
   	if current_user
-  		#to do - add geo parameters to these as well
-  		@user_events = current_user.find_attending_events
-  		@suggested_events = current_user.find_suggested_events
-  		@new_rsvp = Rsvp.new
+      geos = UserGeo.where user_id: current_user.id
+      subscriptions = Subscription.where user_id: current_user.id
+      if geos.count > 0 || subscriptions.count > 0
+    		@user_events = current_user.find_attending_events
+    		@suggested_events = current_user.find_suggested_events
+    		@new_rsvp = Rsvp.new
+      else
+        flash[:notice] = "Please complete your account preferences below"
+        redirect_to define_profile_path
+      end
 	else
 		redirect_to log_in_path#new_user_session_path
 	end
