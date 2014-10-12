3e951c8779333386fd5536eafe4b8bafa1d01f31
Erik Osmond <erikosmond@gmail.com>
2014-10-11 20:57:17 -0400

    update migration to make heroku happy again2

--- db/migrate/20140630125529_change_event_times.rb
+++ db/migrate/20140630125529_change_event_times.rb
@@ -1,7 +1,7 @@
 class ChangeEventTimes < ActiveRecord::Migration
   def change
-  	add_column :events, :starts_at, :date
-  	add_column :events, :ends_at, :date
+  	add_column :events, :starts_at, :time
+  	add_column :events, :ends_at, :time
   	remove_column :events, :start_time
   	remove_column :events, :end_time
   end
--- db/migrate/20140630131307_change_event_time_type.rb
+++ db/migrate/20140630131307_change_event_time_type.rb
@@ -1,6 +1,6 @@
 class ChangeEventTimeType < ActiveRecord::Migration
   def change
-  	change_column :events, :starts_at, :time
-  	change_column :events, :ends_at, :time
+  	#change_column :events, :starts_at, :time
+  	#change_column :events, :ends_at, :time
   end
 end
