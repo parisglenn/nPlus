9455a6a52ac588c0cc9f707873a5f1e2775df0e3
Erik Osmond <erikosmond@gmail.com>
2014-10-11 20:51:49 -0400

    update migration to make heroku happy again

--- db/migrate/20140426162144_change_event_dateto_date_type.rb
+++ db/migrate/20140426162144_change_event_dateto_date_type.rb
@@ -1,6 +1,5 @@
 class ChangeEventDatetoDateType < ActiveRecord::Migration
   def change
-  	remove_column :events, :event_date
-    change_column :events, :event_date, :date
+    add_column :events, :event_date, :date
   end
 end
