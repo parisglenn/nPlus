aa87c8e6afec515f161f089865c3119dd9f255cf
Erik Osmond <erikosmond@gmail.com>
2014-10-11 20:59:53 -0400

    update migration to make heroku happy again2

--- db/migrate/20140522131848_create_create_round_up_times.rb
+++ db/migrate/20140522131848_create_create_round_up_times.rb
@@ -2,8 +2,8 @@ class CreateCreateRoundUpTimes < ActiveRecord::Migration
   def change
     create_table :round_up_times do |t|
       t.string :day
-      t.integer :start_hour
-      t.integer :end_hour
+      t.time :start_hour
+      t.time :end_hour
       t.boolean :deprecated
 
       t.timestamps
